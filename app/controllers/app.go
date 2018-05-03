7d2219abfea8148d3a96791e23d25084d806780c
aokabi <aokabit@gmail.com>
2018-05-03 23:08:49 +0900

    プロトタイプ作った

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"time"
+
 	"github.com/revel/revel"
 )
 
@@ -8,6 +10,31 @@ type App struct {
 	*revel.Controller
 }
 
+type entry struct {
+	Day     time.Time
+	Content string
+}
+
+var (
+	e []entry
+)
+
+const (
+	layout = "2006-01-02"
+)
+
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+func (c App) Post() revel.Result {
+	revel.AppLog.Debug(c.Params.Form.Get("content"))
+	d := time.Now()
+	if len(e) == 0 {
+		e = append(e, entry{d, c.Params.Form.Get("content")})
+	} else {
+		e = append([]entry{entry{d, c.Params.Form.Get("content")}}, e...)
+	}
+	c.ViewArgs["entry"] = e
+	return c.RenderTemplate("App/Index.html")
 }
